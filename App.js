f00690385301fa83b978d5e8e89d0720ce7d5868
Michael Woon <michael_woon17@yahoo.com>
2018-10-16 00:12:25 -0400

    Search page created

--- App.js
+++ App.js
@@ -5,10 +5,19 @@
  * @format
  * @flow
  */
+//strict mode has better error handling
+ 'use strict';
 
+//libraries
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  createStackNavigator,
+} from 'react-navigation';
+import SearchPage from './SearchPage';
 
+
+//variable based on platform (no longer needed)
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -16,34 +25,14 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+//component that represents the UI
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
+//configures searchpage component as first on stack
+const App = createStackNavigator({
+  Home: { screen: SearchPage },
+});
+export default App;
+//style object
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
